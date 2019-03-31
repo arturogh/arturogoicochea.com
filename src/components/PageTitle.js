e2cab6774709be4cb85cde3765efc6a9937aeb37
Arturo Goicochea <argoicoc@microsoft.com>
2019-03-31 13:36:18 -0700

    Fresh start with Netlify CMS

--- src/components/PageTitle.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import {ModScale, font, maxWidth} from '../utils';
-
-export const PageTitle = styled.h2`
-  font-size: ${font.getFontData('homeTitle').size};
-  font-weight: ${font.getFontData('homeTitle').weight};
-  color: ${font.getFontData('homeTitle').color};
-  margin: ${ModScale.large} 0 ${ModScale.xSmall};
-`;
--- src/utils/index.js
+++
@@ -1,4 +0,0 @@
-export {maxWidth} from './spacing';
-export {Colors} from './colors';
-export {ModScale} from './modScale.js';
-export {font} from './font';
